3af84cb2aa1304b48e3bf6ac4dfd6519b5dfa055
simplecloudservlet <outrosdiasvirao@gmail.com>
2024-02-26 10:18:36 -0300

    Minor changes

---
+++ db_sqlite.php
@@ -0,0 +1,116 @@
+
+	<?php
+
+	function executarSQL($db, $sql){
+
+		try {
+
+			if($db->exec($sql)){
+				echo "<h3>SQL: [". $sql ."] realizada com sucesso!</h3>";
+			} else {
+				echo "<h3>ERRO na SQL: ". $sql ." </h3>";
+			}
+
+		} catch(PDOException $e){
+			echo '<h3>EXCEPTION1: ' . $e->getMessage() . '</h3>';
+		}
+	}
+
+	function imprimir($db){
+		$res = $db->query("SELECT * FROM produtos");
+		if($res){
+			$res->setFetchMode(PDO::FETCH_OBJ);
+	
+			while( $tupla = $res->fetch() ){ //recupera uma linha por vez
+				foreach($tupla as $coluna){
+					echo '<h3>' . $coluna . "</h3>";
+				}
+				echo '<br>';
+			}
+			echo "<h3>SELECT: Consulta realizada com sucesso!</h3>";
+		} else {
+			echo "<h3>ERRO6: Erro na consulta.</h3>";
+		}
+	}
+
+	//function criarBD(){
+		
+	//}
+
+	function atualizarTupla($db){
+
+		echo '<div class="selecionado"><h3>TODO11</h3>';
+		
+		executarSQL($db, "UPDATE produtos SET nome='Caderno' WHERE nome='Borracha'");
+		executarSQL($db, "SELECT * FROM produtos");
+		
+		echo '</div>';
+	}
+
+	function inserirTupla($db){
+
+		echo '<div class="selecionado"><h3>TODO5</h3>';
+
+		executarSQL($db, "INSERT INTO produtos VALUES ( 1, 'Lápis')");
+		executarSQL($db, "INSERT INTO produtos(id,nome) values ( 2, 'Borracha')");
+
+		echo '</div>';
+	}
+
+	function limparTabela($db){
+
+		echo "<h3>TODO12</h3><br>";
+		executarSQL($db, "delete from produtos where id > 0");
+		
+	}
+
+	function criarTabela($db){
+
+		echo '<div class="selecionado"><h3>TODO2</h3>';
+		executarSQL($db, "CREATE TABLE produtos( id int primary key not null, nome varchar(50) not null)"); 
+		echo '</div>';
+	}
+
+	function main(){
+		//criarBD();
+
+		echo '<h3>TODO1</h3>';
+		//$DATABASE = "mysql";
+		$DATABASE = "sqlite"; //Não executa com PHP -S localhost:... Utilizar o servidor Apache2
+		$HOST = "localhost";
+		$DBNAME = "papelaria"; //mysql> create database papelaria;
+		$USER = "lucio";
+		$PASSWORD = "root";
+
+		try {
+			//	$db = new PDO("$DATABASE: host=$HOST; dbname=$DBNAME", $USER, $PASSWORD); //Para o MySQL
+			$db = new PDO("$DATABASE:teste.db"); //Para o SQLite
+			var_dump($db);
+		} catch(PDOException $e){
+			echo '<h3>EXCEPTION1: ' . $e->getMessage() . '</h3>';
+		}
+		
+		//TODO2
+		criarTabela($db);
+
+		//TODO12
+		limparTabela($db);
+
+		//TODO5
+		inserirTupla($db);
+		
+		//TODO6
+		imprimir($db);
+
+		//TODO11
+		atualizarTupla($db);
+
+		//TODO6
+		imprimir($db);
+
+	}
+
+		//Invoca as funcoes
+		main();
+	?>
+	
